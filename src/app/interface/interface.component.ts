53a8d2ba8a58dd349f212dce5644c33c457ad705
ErykKul <eryk.kulikowski@gmail.com>
2026-04-25 00:54:52 +0200

    Fix dvLocale query separator when callback URL has no query string

--- src/app/interface/interface.component.ts
+++ src/app/interface/interface.component.ts
@@ -147,7 +147,8 @@ export class InterfaceComponent implements OnInit {
 
     getCode(callback, dvLocale) {
         const decodedCallback = this.decodeCallback(callback);
-        let state = decodedCallback + '&dvLocale=' + dvLocale;
+        const sep = decodedCallback.indexOf('?') === -1 ? '?' : '&';
+        let state = decodedCallback + sep + 'dvLocale=' + dvLocale;
         state = btoa(state);
         const clientId = this.config.globusClientId;
 
